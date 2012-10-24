e1e7a2f50789ac81ed66e234bf8ada4c2efd60fd
Shen Chen <scv119@gmail.com>
2012-10-25 00:40:10 +0800

    add lis in nlgn

--- src/facebook/LIS.java
+++ src/facebook/LIS.java
@@ -36,12 +36,47 @@ public class LIS {
         return dp[len -1];
     }
 
+    //nlgn solution
+    public static int solve1(int[] arr) {
+        if(arr == null || arr.length == 0)
+            return 0;
+        if(arr.length == 1)
+            return 1;
+
+        int len = arr.length;
+        int stack[] = new int[len];
+        int stackLen = 0;
+
+        Arrays.fill(stack, Integer.MAX_VALUE);
+        stack[stackLen ++ ] = arr[0];
+        for(int i = 1; i < len; i ++) {
+            if(arr[i] > stack[stackLen - 1])
+                stack[stackLen ++] = arr[i];
+            else if(stack[0] >= arr[i]) {
+                stack[0] = arr[i];
+            } else {
+                int start = 0; int end = stackLen -1;
+                while(start < end - 1) {
+                    int mid = start + (end - start)/2;
+                    if(stack[mid] >= arr[i])
+                        end = mid;
+                    else
+                        start = mid;
+                }
+                stack[end] = arr[i];
+            }
+        }
+        return stackLen;
+    }
+
+
+
     public static void main(String args[]) {
-        System.out.println(LIS(new int[]{1,2,3,4,9,5}));
+        System.out.println(solve1(new int[]{1,2,3,3,4,9,5}));
 
-        System.out.println(LIS(new int[]{1}));
+        System.out.println(solve1(new int[]{1}));
 
-        System.out.println(LIS(new int[]{}));
+        System.out.println(solve1(new int[]{}));
 
     }
 }
