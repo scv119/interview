078b99ba6f1e0917a51554e5eb0eedaa536a52ce
Shen Chen <scv119@gmail.com>
2012-10-24 21:35:25 +0800

    add KMP

--- src/facebook/KMP.java
+++ src/facebook/KMP.java
@@ -8,36 +8,45 @@ package facebook;
  * To change this template use File | Settings | File Templates.
  */
 public class KMP {
-    public static int strstr(char s[], char p[]) {
-        if(p == null || p.length == 0 || s == null || s.length == 0)
-            return -1;
-        int ret = -1;
+    public static int match(char[] s, char p[]) {
+        int[] next = build(p);
         int j = 0;
-        int next[] = next(p);
-        for(int i = 0 ; i < s.length ; i ++) {
+        for(int i = 0; i < s.length; i ++) {
+            while(s[i] != p[j]){
+                if(j > 0 && next[j-1] > 0)
+                    j = next[j-1];
+                else
+                    break;
+            }
             if(s[i] == p[j])
                 j ++;
             else
-                while(j != 0) {
-                    j = next[j-1];
-
-                }
+                j = 0;
+            if(j == p.length)
+                return i - p.length + 1;
         }
-        return ret;
+        return -1;
     }
 
-    private static int[] next(char[] arr ) {
-        int[] next = new int[arr.length];
-        next[0] = -1;
-        for(int i = 1; i < next.length; i++) {
-            char c = arr[i];
-            int idx = i - 1;
-            while(c != arr[next[idx]] && idx != -1)
-                idx = next[idx];
-            if(idx == 0 && arr[0] == c)
-                next[i] = 1;
-            else if(idx != 0)
-                next[i] = next[idx] + 1;
+    private static int[] build(char p[]) {
+        int[] next = new int[p.length];
+        next[0] = 0;
+        for(int i = 1; i < p.length; i ++) {
+            int k = next[i - 1];
+            while(k != 0) {
+                if(p[k] == p[i])
+                    break;
+                k = next[k - 1];
+            }
+
+            if(k == 0) {
+                if(p[0] == p[i])
+                    next[i] = 1;
+                else
+                    next[i] = 0;
+            } else {
+                next[i] = k + 1;
+            }
         }
         return next;
     }
@@ -50,7 +59,6 @@ public class KMP {
     }
 
     public static void main(String args[]) {
-        int[] next = next("abcabcdabcadabcabcdabc".toCharArray());
-        showArray(next);
+        System.out.println(match("aacaacaaa".toCharArray(), "aaa".toCharArray()));
     }
 }
