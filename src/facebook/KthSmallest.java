f0ec272dd9f00fd49d13801f1b4d81835c05c83c
Shen Chen <scv119@gmail.com>
2012-11-06 23:00:20 +0800

    add

--- src/facebook/KthSmallest.java
+++ src/facebook/KthSmallest.java
@@ -15,89 +15,36 @@ find median/kth smallest from 2 sorted array
 
 public class KthSmallest {
 
-    public static class SolveException extends RuntimeException {
-        SolveException(String message) {
-            super(message);
-        }
+    public double findMedianSortedArrays(int A[], int B[]) {
+        int lenA = A.length;
+        int lenB = B.length;
+        int len  = (lenA + lenB);
+        double ret = 0;
+        ret = findK(A, 0, lenA, B, 0, lenB, (len + 1) / 2);
+        if (len%2 == 0)
+            ret = (ret + findK(A, 0, lenA, B, 0, lenB, (len + 1) / 2 + 1)) / 2;
+        return ret;
     }
 
-    public static int solve(int arr1[], int arr2[], int k) {
-        if (arr1 == null || arr2 == null || k > arr1.length + arr2.length)
-            throw new SolveException("bad argument");
-        return solve(arr1, 0, arr1.length, arr2, 0, arr2.length, k);
-    }
+    int findK(int A[], int startA, int lenA, int B[], int startB, int lenB, int k) {
+        int p1 = (int)(1.0f * lenA / (lenA + lenB) * (k - 1));
+        int p2 = k - 1 - p1;
 
-    private static int solve(int arr1[], int start1, int len1, int arr2[], int start2, int len2, int k) {
-        if (len1 == 0)
-            return arr2[start2 + k - 1];
-        else if (len2 == 0)
-            return arr1[start1 + k - 1];
+        int a1 = p1 == 0 ? Integer.MIN_VALUE : A[startA + p1 - 1];
+        int a2 = p1 == lenA ? Integer.MAX_VALUE : A[startA + p1];
 
-        if (k == 1)
-            return Math.min(arr1[start1], arr2[start2]);
-        else if(len1 == 1) {
-            return solve(arr2, start2, len2, arr1[start1], k);
-        } else if(len2 == 1) {
-            return solve(arr1, start1, len1, arr2[start2], k);
-        }
+        int b1 = p2 == 0 ? Integer.MIN_VALUE : B[startB + p2 - 1];
+        int b2 = p2 == lenB ? Integer.MAX_VALUE : B[startB + p2];
 
-        int pivot1 = 0;
-        int pivot2 = 0;
+        if (a1 <= b2 && b2 <= a2)
+            return b2;
+        if (b1 <= a2 && a2 <= b2)
+            return a2;
 
-        int mid = (k - 2)/2;
-        if(len1 < len2) {
-            if(mid < len1)
-                pivot1 = mid;
-            else
-                pivot1 = len1 - 1;
-            pivot2 = k - 2 - pivot1;
-        } else {
-            if(mid < len2)
-                pivot2 = mid;
-            else
-                pivot2 = len2 - 1;
-            pivot1 = k - 2 - pivot2;
-        }
-
-        int value1 = arr1[start1 + pivot1];
-        int value2 = arr2[start2 + pivot2];
-        if(value1 == value2)
-            return Math.max(value1, value2);
-        else if(value1 < value2)
-            return solve(arr1, start1 + pivot1, len1 - pivot1 ,arr2, start2, pivot2 + 1 ,k - pivot1 );
+        if (a2 < b2)
+            return findK(A, startA + p1 + 1, lenA - p1 - 1, B, startB, p2, k - p1 - 1);
         else
-            return solve(arr2, start2 + pivot2, len2 - pivot2 ,arr1, start1, pivot1 + 1 ,k - pivot2 );
-
-    }
-
-    private static int solve(int arr1[], int start1, int len1, int value, int k) {
-        int value1 = arr1[start1 + k - 2];
-        if(value1 >= value)
-            return value1;
-        else if(start1 + k - 1 >= start1 + len1)
-            return value;
-        else return Math.min(value, arr1[start1 + k -1]);
-    }
-
-    public static void main(String args[]) {
-
-        int[] arr1 = new int[50];
-        int[] arr2 = new int[50];
-        for(int i = 0; i < 50 ;i ++) {
-            arr1[i] = i;
-            arr2[i] = 50 + i;
-        }
-        for(int i = 1; i <= 100; i ++) {
-            assert(solve(arr1, arr2, i) == i - 1);
-        }
-
-        for(int i = 0; i < 50 ;i ++) {
-            arr1[i] = 2*i;
-            arr2[i] = 2*i + 1;
-        }
-        for(int i = 1; i <= 100; i ++) {
-            assert(solve(arr1, arr2, i) == i - 1);
-        }
+            return findK(A, startA, p1, B, startB + p2 + 1, lenB - p2 - 1, k - p2 - 1);
     }
 
 }
